9ff05a1bc30382c8d1e8ee65fc82e4d0eef4d7e7
mr-gukas <gukasyan.vladimir@mail.ru>
2022-08-17 17:49:13 +0300

    Первая версия квадратки

---
+++ square.cpp
@@ -0,0 +1,78 @@
+#include <TXLib.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+const int INFIN = -1;
+bool NearZero(double x);
+void SolveTheSquare(double quadr, double line, double free);
+void ShowSolution(int count, double * x1, double * x2);
+
+int main(void)
+{
+    double quadr = 0.0, line = 0.0, free = 0.0;
+
+    puts("Введите по порядку квадратный, линейный и свободный коэффициенты квадратного уравнения:");
+    while (scanf("%lf %lf %lf", &quadr, &line, &free) == 3) {
+        SolveTheSquare(quadr, line, free);
+        puts("Введите по порядку квадратный, линейный и свободный коэффициенты квадратного уравнения");
+        puts("(или иные символы для выхода из программы):");
+    }
+    puts("Программа завершена");
+
+    return 0;
+}
+void SolveTheSquare(double quadr, double line, double free)
+{
+    double x1 = 0.0, x2 = 0.0;
+    int count = 0;
+
+    if (NearZero(quadr))
+    {
+        if (NearZero(line))
+            count =  (NearZero(free)) ? INFIN : 0;
+        else
+        {
+            x1 = -free / line;
+            if (NearZero(x1))
+                x1 = 0.0f;
+            count = 1;
+        }
+    }
+    else
+    {
+        double discr = line * line - 4 * quadr * free;
+        if (discr < 0.0f)
+            count = 0;
+        else
+        {
+            x1 = (-line + sqrt(discr)) / (2 * quadr);
+            x2 = (-line - sqrt(discr)) / (2 * quadr);
+            count = (fabs(x1 - x2) <= 0.00001) ? 1 : 2;
+        }
+    }
+    ShowSolution(count, &x1, &x2);
+}
+
+bool NearZero(double x)
+{
+    return (fabs(x) < 0.00001) ? true : false;
+}
+
+void ShowSolution(int count, double * x1, double * x2)
+{
+    switch (count)
+    {
+        case 0: puts("Данное уравнение не имеет решений");
+                break;
+        case 1: printf("Данное уравнение имеет единственное решение: x = %lf\n", *x1);
+                break;
+        case 2: printf("Данное уравнение имеет два решения:\n");
+                printf("    x1 = %lf\n", *x1);
+                printf("    x2 = %lf\n", *x2);
+                break;
+        case INFIN: puts("Данное уравнение имеет бесконечное множество решений");
+                    break;
+        default: break;
+    }
+}
